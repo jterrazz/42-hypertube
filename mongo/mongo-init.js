0e66693fbe8b0ade9b94cc593f7968f03a854f28
Terrazzoni Jean-Baptiste <jterrazz@protonmail.com>
2019-10-16 10:49:50 +0200

    mongo rules

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -11,3 +11,5 @@ db.createUser({
 
 db.users.createIndex({ username: 1 }, { unique: true })
 db.users.createIndex({ email: 1 }, { unique: true })
+db.movies.createIndex({ imdbId: 1 }, { unique: true })
+db.torrents.createIndex({ hash: 1 }, { unique: true })
