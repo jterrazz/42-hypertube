e53694e718ca2dd5b62e727ea72d5106e6b90ff2
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-11-27 08:41:00 +0100

    doc

--- web/utils/i18n.js
+++ web/utils/i18n.js
@@ -2,6 +2,11 @@ const nextI18next = _interopRequireDefault(require("next-i18next"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/*
+ * i18n package for NextJS
+ * https://github.com/isaachinman/next-i18next
+ */
+
 const NextI18NextInstance = new nextI18next.default({
   defaultLanguage: 'fr-FR',
   otherLanguages: ['en-US']
