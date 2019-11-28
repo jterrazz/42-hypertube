f9b3b1dedbba46c368e25e2e7179cc7b0508a80c
Abdessalem BENSIDI AISSA <abbensid@e1r6p8.42.fr>
2019-11-28 12:49:26 +0100

    update 3rd party auth in /login

--- web/components/organisms/GroupButtonOauth.js
+++ web/components/organisms/GroupButtonOauth.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid'
-import FacebookIcon from '@material-ui/icons/Facebook'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import AccountBoxIcon from '@material-ui/icons/AccountBox'
+import Grid from '@material-ui/core/Grid';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import PersonalVideoIcon from '@material-ui/icons/PersonalVideo'
 import { OauthURL } from "../../config/OauthURL";
 import { BoxButtonOauth } from "../molecules/BoxButtonOauth";
@@ -11,7 +11,7 @@ export const GroupButtonOauth = () => {
   return (
     <Grid container justify="center">
       <BoxButtonOauth text="Facebook" icon={<FacebookIcon />} href={OauthURL.facebook}/>
-      <BoxButtonOauth text="LinkedIn" icon={<LinkedInIcon />} href={OauthURL.linkedIn}/>
+      <BoxButtonOauth text="Github" icon={<GitHubIcon />} href={OauthURL.github}/>
       <BoxButtonOauth text="Google" icon={<AccountBoxIcon />} href={OauthURL.google}/>
       <BoxButtonOauth text="Intra_42" icon={<PersonalVideoIcon />} href={OauthURL.intra_42}/>
     </Grid>
--- web/config/OauthURL.js
+++ web/config/OauthURL.js
@@ -1,6 +1,5 @@
 export const OauthURL = {
   facebook: 'http://localhost:3000/auth/facebook',
-  linkedIn: 'http://localhost:3000/auth/linkedin',
   google: 'http://localhost:3000/auth/google',
   github: 'http://localhost:3000/auth/github',
   intra_42: 'http://localhost:3000/auth/42',
